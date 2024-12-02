1098402bfb0179f6dd0badf6d43d40a435a6a18c
zyhou <richard.maxime9@gmail.com>
2024-12-02 20:54:34 +0100

    fix CI

--- 2024/02/index.ts
+++ 2024/02/index.ts
@@ -6,7 +6,7 @@ type Demand = 900_000;
 
 type t0_actual = Demand; // =>
 type t0_expected = 900_000; // =>
-type t0 = Expect<Equal<t0_actual, t0_expected>>;
+type test_t0 = Expect<Equal<t0_actual, t0_expected>>;
 
 /*
  * Sometimes, throughout Advent of TypeScript, you'll see a test that asserts there _is_ a type error, often to ensure that you're providing a correct implementation.
