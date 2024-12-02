b08d0454405bb7b1721010fd3f018aa9d8d34615
zyhou <richard.maxime9@gmail.com>
2024-12-02 23:48:13 +0100

    fix eslint/prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,34 @@
-// @ts-check
-
+import globals from "globals";
 import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: ["node_modules", "pnpm-lock.yaml"],
+  },
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
 
   {
-    files: ["**/*.ts"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.ts", "eslint.config.js"],
+          defaultProject: "./tsconfig.json",
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       "@typescript-eslint/no-import-type-side-effects": "error",
       "@typescript-eslint/consistent-type-imports": [
@@ -30,4 +49,6 @@ export default tseslint.config(
       ],
     },
   },
+
+  prettierConfig,
 );
